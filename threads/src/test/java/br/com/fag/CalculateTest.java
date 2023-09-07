1722671701a9e0dc32bb33925eb87620d4106bb2
Guilherme Perinotti <guilherme9115@hotmail.com>
2023-09-07 20:24:16 -0300

    implementing calculate class

--- threads/src/test/java/br/com/fag/CalculateTest.java
+++ threads/src/test/java/br/com/fag/CalculateTest.java
@@ -1,19 +1,20 @@
 package br.com.fag;
 
+import java.io.FileNotFoundException;
+
 import br.com.fag.adapter.Enterprise;
 import br.com.fag.adapter.Filial;
 
-public class Calculate {
-
-  public Float calculate(Enterprise[] listaEmpresas) {
+public class CalculateTest {
+  public static void main(String[] args) throws FileNotFoundException {
     Float result = 0f;
+    Enterprise[] listaEmpresas = new JsonParser("data1.json").parse();
     for (Enterprise empresa : listaEmpresas) {
       for (Filial filial : empresa.getFiliais()) {
         result += filial.getVendaMensal();
       }
     }
-    
-    return result;
+
+    System.out.println(result);
   }
-  
 }
