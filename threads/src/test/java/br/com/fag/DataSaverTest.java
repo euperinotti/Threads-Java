4195cbbe0a788b939a165bd54edc5691f3dfcfb9
Guilherme Perinotti <guilherme9115@hotmail.com>
2023-09-07 20:37:23 -0300

    implementig data saver

---
+++ threads/src/test/java/br/com/fag/DataSaverTest.java
@@ -0,0 +1,31 @@
+package br.com.fag;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+
+import com.google.gson.Gson;
+
+public class DataSaverTest {
+  public static void main(String[] args) {
+    File newJson = new File("threads/src/main/java/br/com/fag/data/result.json");
+    Gson gson = new Gson();
+    JsonParser json = new JsonParser("data1.json");
+
+    FileWriter writer;
+    try {
+      writer = new FileWriter(newJson);
+      writer.write(gson.toJson(json.parse()));
+      writer.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    try {
+      newJson.createNewFile();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
