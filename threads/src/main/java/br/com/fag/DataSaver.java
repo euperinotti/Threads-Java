40d7fad1aa1526c103cc3e3e5d96eefe116b26a6
Guilherme Perinotti <guilherme9115@hotmail.com>
2023-09-11 09:08:10 -0300

    fixed filename

--- threads/src/main/java/br/com/fag/DataSaver.java
+++ threads/src/main/java/br/com/fag/DataSaver.java
@@ -3,14 +3,16 @@ package br.com.fag;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 import com.google.gson.Gson;
 
-import br.com.fag.adapter.receive.Enterprise;
+import br.com.fag.adapter.save.EnterpriseResult;
 
 public class DataSaver {
-  public void save(Enterprise[] listaEnterprises) {
-    File newJson = new File("threads/src/main/java/br/com/fag/data/result.json");
+  public void save(ArrayList<EnterpriseResult> listaEnterprises) {
+    File newJson = new File("threads/src/main/java/br/com/fag/data/" + LocalDateTime.now().toLocalDate().toString() + Math.random() + ".json");
     Gson gson = new Gson();
     FileWriter writer;
     try {
